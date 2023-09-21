f50b752223233e2b839dc03fe07e395ac83f9255
Amirhosein Qadamgahi <amirhoseinqadamgahi@gmail.com>
2023-09-21 12:13:06 +0330

    removed extra cli related files

--- bin/index.js
+++ bin/index.js
@@ -1,27 +1,12 @@
 #!/usr/bin/env node
 const { program } = require('commander');
 const initCommand = require('./lib/cli/init');
-const createCommand = require('./lib/cli/create');
 
 program
     .command('init')
     .description('Genereates Config File')
     .action(() => {
         initCommand();
-    });
-
-program
-    .command('create')
-    .description('Generates CSS File Based On Configs')
-    .action(() => {
-        createCommand();
-    });
-
-program
-    .command('update')
-    .description('Updates CSS Files Based On Changes On Config File')
-    .action(() => {
-        updateCommand();
     });
 
 program
